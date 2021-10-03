0701ab1f2ab8732d997fb7ddc4fc76f9cf59d1e6
tomioka-k <tomioka-k@tajima.co.jp>
2021-10-04 02:58:42 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,149 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container, Grid, TextField } from "@mui/material";
+import React, { useState } from "react";
+import "./App.css";
+import { Seminar } from "./components/types";
+
+const initialstate: Seminar = {
+  name: "",
+  description: "",
+  linkUrl: "",
+  overview: "",
+  overviewDescription: "",
+  start: "",
+  place: "",
+  entryFee: "",
+  attendUrl: "",
+  deadline: "",
+  flyerUrl: "",
+  inquiry: "",
+};
 
 function App() {
+  const [seminar, setSeminar] = useState(initialstate);
+
+  const handleInuptChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const key: string = e.target.name;
+    const value = e.target.value;
+    setSeminar({ ...seminar, [key]: value });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Grid container spacing={5}>
+          <Grid item xs={5}>
+            <TextField
+              fullWidth
+              label="セミナー名"
+              type="text"
+              name="name"
+              variant="standard"
+              value={seminar.name}
+              onChange={handleInuptChange}
+            />
+            <TextField
+              fullWidth
+              label="説明文"
+              multiline
+              type="text"
+              name="description"
+              variant="standard"
+              value={seminar.description}
+              onChange={handleInuptChange}
+            />
+            <TextField
+              fullWidth
+              label="リンクURL"
+              type="text"
+              name="linkUrl"
+              variant="standard"
+              value={seminar.linkUrl}
+              onChange={handleInuptChange}
+            />
+            <TextField
+              fullWidth
+              label="概要"
+              type="text"
+              name="overview"
+              variant="standard"
+              value={seminar.overview}
+              onChange={handleInuptChange}
+            />
+            <TextField
+              fullWidth
+              multiline
+              label="概要説明"
+              type="text"
+              name="overviewDescription"
+              variant="standard"
+              value={seminar.overviewDescription}
+              onChange={handleInuptChange}
+            />
+            <TextField
+              fullWidth
+              label="日時"
+              type="text"
+              name="start"
+              variant="standard"
+              value={seminar.start}
+              onChange={handleInuptChange}
+            />
+            <TextField
+              fullWidth
+              label="会場"
+              type="text"
+              name="place"
+              variant="standard"
+              value={seminar.place}
+              onChange={handleInuptChange}
+            />
+            <TextField
+              fullWidth
+              label="費用"
+              type="text"
+              name="entryFee"
+              variant="standard"
+              value={seminar.entryFee}
+              onChange={handleInuptChange}
+            />
+          </Grid>
+          <Grid item xs={7}>
+            <p>
+              表題： <br />
+              ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+              <br />
+              ┏━┳┓
+              <br />
+              ┣┳┫┃ {seminar.name ? seminar.name : "[セミナー名]"} <br />
+              ┃┣┻┫ {seminar.linkUrl} <br />
+              ┗┻━┛ <br />
+              ━━━━━━━━━━━━━━━━━━━田島ルーフィング株式会社 ━━━
+              <br />
+              <br />
+              {seminar.description}
+              <br />
+              <br />
+              ┏┓━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+              <br />
+              ┗□ セミナー概要
+              <br />
+              ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+              <br />
+              {seminar.overview}
+              <br />
+              {seminar.overviewDescription}
+              <br />
+              <br />
+              <br />
+              <li>日時：{seminar.start}</li>
+              <br />
+              <li>会場：{seminar.place}</li>
+              <br />
+              <li>費用：{seminar.entryFee}</li>
+            </p>
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 }
---
+++ src/components/types.ts
@@ -0,0 +1,14 @@
+export interface Seminar {
+  name: string;
+  description: string;
+  linkUrl: string;
+  overview: string;
+  overviewDescription: string;
+  start: string;
+  place: string;
+  entryFee: string;
+  attendUrl: string;
+  deadline: string;
+  flyerUrl: string;
+  inquiry: string;
+}
